143061094f6ab2cd62b972b949fe0d03146d8a4a
Mauricio <=>
2019-08-16 13:31:09 +0100

    publishing

---
+++ precache-manifest.62aa5da9c8dd07afc159a8576461db4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "481ae24745793aec7527c79647b014a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55d4ea151343e93c5af8",
+    "url": "/static/css/main.7c7f7d12.chunk.css"
+  },
+  {
+    "revision": "1abe6377af2e0aecc632",
+    "url": "/static/js/2.4ce4b918.chunk.js"
+  },
+  {
+    "revision": "55d4ea151343e93c5af8",
+    "url": "/static/js/main.ed7352ee.chunk.js"
+  },
+  {
+    "revision": "fa61e5cfcd390698906e",
+    "url": "/static/js/runtime~main.ec45910d.js"
+  }
+]);
